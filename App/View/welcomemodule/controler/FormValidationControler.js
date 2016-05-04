6a2ec60cac1e79e925d37faea0c7e0a0e52ef1e2
Nuevo <leramos62@gmail.com>
2016-05-04 20:04:41 -0300

    version 1.3

---
+++ App/View/welcomemodule/controler/FormValidationControler.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Luisito la maravilla on 02-05-16.
+ */
+<!-- creacion de modulos-->
+<!-- primero string y despues dependencias-->
+var app= angula.module('LoginFormModule', []);
+
+<!-- creacion de controles-->
+
+app.controller('LoginFormControler',function($scope,$hhtp)
+        {
+            
+        }
+);
